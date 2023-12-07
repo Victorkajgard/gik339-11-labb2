e672166ff62267d21148b4aaa4047f463279d910
Victorkajgard <113024813+Victorkajgard@users.noreply.github.com>
2023-12-07 15:01:15 +0100

    klart typ

---
+++ client/script.js
@@ -0,0 +1,39 @@
+const url = "http://localhost:3000/users";
+const request = new Request(url);
+
+fetch(request)
+  .then((response) => response.json())
+  .then((users) => {
+    console.log(users);
+    const ul = document.createElement("ul");
+    document.body.appendChild(ul);
+    users.forEach((user) => {
+      const html = `<li style="background-color: ${user.color}">
+        Namn: ${user.firstName} ${user.lastName}
+        Användarnamn: ${user.username}
+    </li>`;
+      ul.insertAdjacentHTML("beforeend", html);
+    });
+  });
+
+// responsePromise
+//   .then((response) => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then((users) => console.log(users));
+// const request = new Request(url);
+// fetch(request)
+//   .then((response) => response.json())
+//   .then((users) => {
+//     users.forEach((user) => {
+//       const html = `<p>
+//           ${user.firstName} ${user.lastName}
+//           <span>${user.username}</span>
+//       </p>`;
+
+//       document.body.insertAdjacentHTML('beforeend', html);
+//     });
+//     return 'hej';
+//   })
+//   .then((data) => console.log(data));
